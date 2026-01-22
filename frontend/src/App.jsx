e9df19f800479887663856cbcfd4f23884a25418
Arry <arifquerry@gmail.com>
2026-01-22 14:26:28 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { ReactRouterProvidor } from 'react-router'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
-
-  return (
-    <ReactRouterProvidor>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </ReactRouterProvidor>
-  )
+    return (
+        <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,18 @@
-import React from "react";
 
 const SignUp = () => {
-    return <div>SignUp</div>;
+    // const primaryColor="#f25a13"
+    const primaryColor="#ff4d2d"
+    const hoverColor="#e64323"
+    const bgColor="#fff9f6"
+    const borderColor="#ddd"
+
+    return (
+        <div className="min-h-screen w-full flex items-center justify-center p-4" style={{backgroundColor:bgColor }}>
+            <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
+                
+            </div>
+        </div>
+    );
 };
 
 export default SignUp;
