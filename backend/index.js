0806c893d2f0fe0aa7b2a85e4716c183585c5d49
Arry <91861463+arry043@users.noreply.github.com>
2026-03-02 09:46:07 +0530

    Set trust proxy for Express app

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ import { Server } from "socket.io";
 import { socketHandler } from "./socket.js";
 
 const app = express();
+app.set("trust proxy", 1);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
